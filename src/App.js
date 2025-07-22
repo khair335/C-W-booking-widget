999008c6f172b0107922624d2f8e675013c45ae4
Khair336 <abulkhair6363@gmail.com>
2025-07-22 19:06:31 +0600

    Update Stripe integration to use environment variable for publishable key, enhancing security and configurability.

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function AppContent() {
 
 // Make sure to call `loadStripe` outside of a component's render to avoid
 // recreating the `Stripe` object on every render.
-const stripePromise = loadStripe('pk_test_51L1LUkCdaLiCtPNZgNYqZ9z0GEVKqa9SHtw8G3xMMBtJAPrlR9jJQpUh3HMQKSGZjWAuK35HlMzN76QbcrnBuF5w00qUa6eAMj');
+const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}`);
 
 function App() {
   return (
