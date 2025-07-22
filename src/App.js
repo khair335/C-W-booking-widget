ce4183d2f9ba909f02913f28d15eed366b8da44d
Khair336 <abulkhair6363@gmail.com>
2025-07-22 19:07:14 +0600

    Remove unnecessary blank line in App.js for cleaner code structure.

--- src/App.js
+++ src/App.js
@@ -46,6 +46,7 @@ function AppContent() {
   return <Router />;
 }
 
+
 // Make sure to call `loadStripe` outside of a component's render to avoid
 // recreating the `Stripe` object on every render.
 const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}`);
